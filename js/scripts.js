20b246048cd648121cc9ee601b141841ede981c2
Max Osborne <BurntAluminum@gmail.com>
2018-12-11 15:45:02 -0800

    This commit makes a little clicky thing that teaches HTML in a future commit maybe it will make the condescending message after many clicks.

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  $("h1").click(function() {
+    alert("Header 1, use it for stuff like titles, type as <h1></h1>, enter something like 'Attack of the Booger People'.");
+    alert("FINE, I'll use Laymans terms: THIS is a HEADER. HEADER! It makes BIG WORDS instead of small words...");
+  });
+
+    $("p").click(function() {
+      alert("This is a Paragraph, use it for text and it will auto-format. type as <p></p> This should be something like a wall of text no one will read.");
+      alert("PARAGRAPH is a LONG word that means LOTS OF WORDS.");
+  });
+
+    $("img").click(function() {
+      alert("It's DOOMGUY, the pinnacle of all KAWAII SUGOI-DESU-NE. This is also an image tag, when using the image directory as a source it is typed as <img src='img/urmom.jpg'> referencing your image folder, you can link a URL to urmom.jpg as well.");
+      alert("Okay if you don't at least Know who Doomguy is I'm not helping you, stop talking to me loser.");
+  });
+});
